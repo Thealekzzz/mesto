40f914ee7202454d5ea7e93996ce376a8f301987
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-02-09 13:09:09 +0300

    fix all issues according to brief and checklist

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,36 @@
 const editButton = document.querySelector(".profile__edit-button")
-const closeButton = document.querySelector(".edit-popup__close-button")
+const closeButton = document.querySelector(".popup__close-button")
 
-const popupOverlay = document.querySelector(".page-overlay")
-const popup = document.querySelector(".edit-popup")
+const popupOverlay = document.querySelector(".popup")
+const popup = document.querySelector(".popup__container")
 
-const inputs = document.querySelectorAll(".edit-popup__input")
+const inputs = document.querySelectorAll(".popup__input")
 
 const profileName = document.querySelector(".profile__name")
-const profileStatus = document.querySelector(".profile__description")
+const profileAbout = document.querySelector(".profile__about")
 
-console.log(inputs)
 
+// Открытие попапа по нажатию на кнопку edit
 editButton.addEventListener("click", () => {
-    popupOverlay.classList.remove("hidden")
+    popupOverlay.classList.add("popup_opened")
 
     inputs[0].value = profileName.textContent
-    inputs[1].value = profileStatus.textContent
+    inputs[1].value = profileAbout.textContent
 
 })
 
+// Закрытие попапа без сохранения данных по нажатию на кнопку закрыть
 closeButton.addEventListener("click", () => {
-    popupOverlay.classList.add("hidden")
+    popupOverlay.classList.remove("popup_opened")
 
 })
 
+// Закрытие попапа с сохранением данных по нажатию на кнопку сохранить
 popup.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    popupOverlay.classList.add("hidden")
-    profileName.textContent = inputs[0].value
-    profileStatus.textContent = inputs[1].value
+    popupOverlay.classList.remove("popup_opened")
+    profileName.textContent = inputs[0].value.trim()
+    profileAbout.textContent = inputs[1].value.trim()
 
 })
