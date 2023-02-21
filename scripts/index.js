2a109d828c1cdd5e0d59478a2b8d7bf96997787f
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-02-21 21:11:13 +0300

    feature Added popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,152 @@
 const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".popup__close-button");
+const addButton = document.querySelector(".profile__add-button");
 
-const popupOverlay = document.querySelector(".popup");
-const popup = document.querySelector(".popup__form");
+const editPopupOverlay = document.querySelector(".popup_type_edit");
+const editPopup = document.querySelector(".popup__form_type_edit");
+const editCloseButton = document.querySelector(".popup__close-button_type_edit");
 
-const inputName = document.querySelector(".popup__input_data_name");
-const inputAbout = document.querySelector(".popup__input_data_about");
+const addPopupOverlay = document.querySelector(".popup_type_new-place");
+const addPopup = document.querySelector(".popup__form_type_new-place");
+const addCloseButton = document.querySelector(".popup__close-button_type_new-place");
+
+const viewPopupOverlay = document.querySelector(".popup_type_view");
+const viewCloseButton = document.querySelector(".popup__close-button_type_view");
+
+const editInputName = editPopup.querySelector(".popup__input_data_name");
+const editInputAbout = editPopup.querySelector(".popup__input_data_about");
+
+const addInputTitle = addPopup.querySelector(".popup__input_data_title");
+const addInputImageUrl = addPopup.querySelector(".popup__input_data_image-url");
 
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__about");
 
+const cardsList = document.querySelector(".cards");
+const cardTemplate = document.querySelector("#card-template").content;
+
+
+const initialCards = [
+    {
+      name: 'Балаклава',
+      link: './images/cards/balaklava.jpg'
+    },
+    {
+      name: 'Мангуп-Кале',
+      link: './images/cards/mangup.jpg'
+    },
+    {
+      name: 'Кача',
+      link: './images/cards/kacha.jpg'
+    },
+    {
+      name: 'Ягодное',
+      link: './images/cards/yagodnoe.jpg'
+    },
+    {
+      name: 'Фиолент, Севастополь',
+      link: './images/cards/fiolent.jpg'
+    },
+    {
+      name: 'Форос, Ялта',
+      link: './images/cards/foros.jpg'
+    }
+]; 
+
 
 function onEditButtonClick() {
-    popupOverlay.classList.add("popup_opened");
+    showPopup(editPopupOverlay);
 
-    inputName.value = profileName.textContent;
-    inputAbout.value = profileAbout.textContent;
+    editInputName.value = profileName.textContent;
+    editInputAbout.value = profileAbout.textContent;
 }
 
-function onCloseButtonClick() {
-    popupOverlay.classList.remove("popup_opened");
+function onEditCloseButtonClick() {
+    closePopup(editPopupOverlay);
 }
 
-function onPopupSubmit(e) {
+function onEditPopupSubmit(e) {
     e.preventDefault();
 
-    onCloseButtonClick()
+    onEditCloseButtonClick();
     
-    profileName.textContent = inputName.value.trim();
-    profileAbout.textContent = inputAbout.value.trim();
+    profileName.textContent = editInputName.value.trim();
+    profileAbout.textContent = editInputAbout.value.trim();
+}
+
+function onAddButtonClick() {
+    showPopup(addPopupOverlay);
+
+}
+
+function onAddCloseButtonClick() {
+    closePopup(addPopupOverlay);
+}
+
+function onAddPopupSubmit(e) {
+    e.preventDefault();
+
+    onAddCloseButtonClick();
+
+    renderCard({
+        name: addInputTitle.value,
+        link: addInputImageUrl.value
+    });
+
+    addInputTitle.value = "";
+    addInputImageUrl.value = "";
+    
+}
+
+function onCardClick() {
+    showPopup(viewPopupOverlay);
+}
+
+function onViewCloseButtonClick() {
+    closePopup(viewPopupOverlay);
+}
+
+function showPopup(popupElement) {
+    popupElement.classList.add("popup_opened");
+}
+
+function closePopup(popupElement) {
+    popupElement.classList.remove("popup_opened");
+}
+
+function renderCard(cardData) {
+    const cardItem = cardTemplate.querySelector(".card-list-item").cloneNode(true);
+
+
+    function likeButtonClicked(e) {
+        e.target.classList.toggle("card__like-button_active");
+    }
+
+    function removeButtonClicked(e) {
+        e.target.closest(".card-list-item").remove();
+    }
+
+    function cardClicked(e) {
+        viewPopupOverlay.querySelector(".popup__image").setAttribute("src", cardData.link);
+        viewPopupOverlay.querySelector(".popup__caption").textContent = cardData.name;
+
+        showPopup(viewPopupOverlay);
+    }
+
+
+    cardItem.querySelector(".card__caption").textContent = cardData.name;
+    cardItem.querySelector(".card__image").setAttribute("src", cardData.link);
+
+    cardItem.addEventListener("click", cardClicked);
+    cardItem.querySelector(".card__like-button").addEventListener("click", likeButtonClicked);
+    cardItem.querySelector(".card__remove-card").addEventListener("click", removeButtonClicked);
+
+    cardsList.insertAdjacentElement("afterbegin", cardItem);
+}
+
+function renderCards(cardsData) {
+    cardsData.forEach(cardData => {
+        renderCard(cardData);
+    });
 }
 
 
@@ -36,7 +154,24 @@ function onPopupSubmit(e) {
 editButton.addEventListener("click", onEditButtonClick)
 
 // Закрытие попапа без сохранения данных по нажатию на кнопку закрыть
-closeButton.addEventListener("click", onCloseButtonClick)
+editCloseButton.addEventListener("click", onEditCloseButtonClick)
 
 // Закрытие попапа с сохранением данных по нажатию на кнопку сохранить
-popup.addEventListener("submit", onPopupSubmit)
+editPopup.addEventListener("submit", onEditPopupSubmit)
+
+
+// Открытие попапа по нажатию на кнопку add
+addButton.addEventListener("click", onAddButtonClick)
+
+// Закрытие попапа без добавления места по нажатию на кнопку закрыть
+addCloseButton.addEventListener("click", onAddCloseButtonClick)
+
+// Закрытие попапа с добавлением места по нажатию на кнопку сохранить
+addPopup.addEventListener("submit", onAddPopupSubmit)
+
+
+// Закрытие попапа просмотра фото
+viewCloseButton.addEventListener("click", onViewCloseButtonClick)
+
+
+renderCards(initialCards);
